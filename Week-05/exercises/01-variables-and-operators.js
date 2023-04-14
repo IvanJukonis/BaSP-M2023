998580c7cd1b92c2969ca62477b8f4b09929341f
unknown <ivanjukonis@hotmail.com>
2023-04-14 12:05:53 -0300

    BaSP-W05-2023: Exercise number 01 (Variables and operators) completed.

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,26 @@
+ //Variables and Operators
+
+// Exercise 1
+
+var firstNumber = 5;
+var secondNumber = 5;
+var result = firstNumber + secondNumber;
+
+console.log(result); // 10
+
+// Exercise 2
+
+var firstText = "Mes";
+var secondText = "si";
+var completedText = firstText + secondText;
+
+console.log(completedText); // Messi
+
+
+// Exercise 3
+
+var lengthFirstText = "Dibu";
+var lengthSecondText = "Martinez";
+var textLength = lengthFirstText.length + lengthSecondText.length;
+
+console.log(textLength) // 12

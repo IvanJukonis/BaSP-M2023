f4d572ba3a3da60b65930f1880441da611065855
unknown <ivanjukonis@hotmail.com>
2023-04-25 21:48:58 -0300

    BaSP-W06-2023: Corrections were made to the neatness of the code.

--- Week-06/js/login.js
+++ Week-06/js/login.js
@@ -26,22 +26,22 @@ function isValidEmail(email) {
 }
 
 var numbers = "0123456789";
-var letters="abcdefghyjklmnñopqrstuvwxyz";
+var letters = "abcdefghyjklmnñopqrstuvwxyz";
 
 function hasSpecialCharacters(text) {
     let spChar = "/[[]#$%^&*()_+-=[]{};':\|,.<>/?]+/! ";
     for (var i = 0; i < text.length; i++) {
-       if (spChar.indexOf(text.charAt(i)) != -1) {
-           return true;
-       }
+        if (spChar.indexOf(text.charAt(i)) != -1) {
+            return true;
+        }
     }
     return false;
 }
 
-function hasLetters(text){
+function hasLetters(text) {
     text = text.toLowerCase();
-    for(i = 0; i < text.length; i++){
-        if (letters.indexOf(text.charAt(i),0) != -1) {
+    for (i = 0; i < text.length; i++) {
+        if (letters.indexOf(text.charAt(i), 0) != -1) {
             return true;
         }
     }
@@ -49,10 +49,10 @@ function hasLetters(text){
 }
 
 function hasNumbers(text){
-    for(i=0; i<text.length; i++){
-       if (numbers.indexOf(text.charAt(i),0)!=-1){
-          return true;
-       }
+    for (i = 0; i < text.length; i++) {
+        if (numbers.indexOf(text.charAt(i), 0) != -1){
+            return true;
+        }
     }
     return false;
 }
@@ -61,65 +61,65 @@ function hasNumbers(text){
 function validateInputEmail() {
     const emailValue = email.value.trim();
     if (emailValue === '') {
-        const textError = "Email is required."
+        const textError = "Email is required.";
         setError(email, textError);
         alertTextEmailError = textError;
-        return alertTextEmailError
+        return alertTextEmailError;
     } else if (!isValidEmail(emailValue)) {
-        const textError = "Provide a valid email address."
+        const textError = "Provide a valid email address.";
         setError(email, textError);
         alertTextEmailError = textError;
-        return alertTextEmailError
+        return alertTextEmailError;
     } else {
         setSuccess(email);
         alertTextEmailSuccess = emailValue;
-        return alertTextEmailSuccess
+        return alertTextEmailSuccess;
     }
 }
 
 function validateInputPassword() {
     const passwordValue = password.value.trim();
     if (passwordValue === '') {
-        const textError = "Password is required."
+        const textError = "Password is required.";
         setError(password, textError);
         return alertTextPasswordError = textError;
     } else if (passwordValue.length < 8 ) {
-        const textError = "Password must be at least 8 character."
-        setError(password, textError)
+        const textError = "Password must be at least 8 character.";
+        setError(password, textError);
         alertTextPasswordError = textError;
-        return alertTextPasswordError
+        return alertTextPasswordError;
     } else if (hasSpecialCharacters(passwordValue) || passwordValue.indexOf(' ') > 0) {
-        const textError = "Password cant contain special characters."
-        setError(password, textError)
+        const textError = "Password cant contain special characters.";
+        setError(password, textError);
         alertTextPasswordError = textError;
-        return alertTextPasswordError
+        return alertTextPasswordError;
     } else if (!hasLetters(passwordValue) === true || !hasNumbers(passwordValue) === true) {
-        const textError = "Password must contain numbers and letters."
-        setError(password, textError)
+        const textError = "Password must contain numbers and letters.";
+        setError(password, textError);
         alertTextPasswordError = textError;
-        return alertTextPasswordError
+        return alertTextPasswordError;
     } else {
         setSuccess(password);
         alertTextPasswordSuccess = passwordValue;
-        return alertTextPasswordSuccess
+        return alertTextPasswordSuccess;
     }
 }
 
 function submitForm() {
-    validateInputEmail()
-    validateInputPassword()
+    validateInputEmail();
+    validateInputPassword();
     const parentEmail = email.parentElement;
     const parentPassword = password.parentElement;
-    const valEmailError = parentEmail.classList.contains("error")
-    const valPasswordError = parentPassword.classList.contains("error")
+    const valEmailError = parentEmail.classList.contains("error");
+    const valPasswordError = parentPassword.classList.contains("error");
     if (valEmailError && valPasswordError) {
-        alert("Login error" + "\n" + alertTextPasswordError + "\n" + alertTextEmailError)
+        alert("Login error" + "\n" + alertTextPasswordError + "\n" + alertTextEmailError);
     } else if (valEmailError) {
-        alert("Email Error" + "\n" + alertTextPasswordSuccess + "\n" + alertTextEmailError)
+        alert("Email Error" + "\n" + alertTextPasswordSuccess + "\n" + alertTextEmailError);
     } else if (valPasswordError) {
-        alert("Password Error" + "\n" + alertTextPasswordError + "\n" + alertTextEmailSuccess)
+        alert("Password Error" + "\n" + alertTextPasswordError + "\n" + alertTextEmailSuccess);
     } else {
-        alert("Welcome to MEGAROCKEYGYM" + "\n" + alertTextPasswordSuccess + "\n" + alertTextEmailSuccess)
+        alert("Welcome to MEGAROCKEYGYM" + "\n" + alertTextPasswordSuccess + "\n" + alertTextEmailSuccess);
     }
 }
 
@@ -127,5 +127,5 @@ email.addEventListener("blur", validateInputEmail);
 email.addEventListener("focus", function e() {setSuccess(email)});
 password.addEventListener("blur", validateInputPassword);
 password.addEventListener("focus", function e() {setSuccess(password)});
-login.addEventListener("click", submitForm)
+login.addEventListener("click", submitForm);
 
--- Week-06/js/sign-up.js
+++ Week-06/js/sign-up.js
@@ -35,38 +35,38 @@ function isValidEmail(email) {
 }
 
 var numbers = "0123456789";
-var letters="abcdefghyjklmnñopqrstuvwxyz";
+var letters = "abcdefghyjklmnñopqrstuvwxyz";
 
 function hasSpecialCharacters(text, space) {
-    var spChar
+    var spChar;
     if (space) {
         var spChar = "/[[]#$%^&*()_+-=[]{};':\|,.<>/?]+/! ";
     } else {
         var spChar = "/[[]#$%^&*()_+-=[]{};':\|,.<>/?]+/!";
     }
     for (var i = 0; i < text.length; i++) {
-       if (spChar.indexOf(text.charAt(i)) != -1) {
-           return true;
-       }
-    }
-    return false;
-}
-
-function hasLetters(text){
-    text = text.toLowerCase();
-    for(i = 0; i < text.length; i++){
-        if (letters.indexOf(text.charAt(i),0) != -1) {
+        if (spChar.indexOf(text.charAt(i)) != -1) {
             return true;
         }
     }
     return false;
 }
 
-function hasNumbers(text){
-    for(i=0; i<text.length; i++){
-       if (numbers.indexOf(text.charAt(i),0)!=-1){
-          return true;
-       }
+function hasLetters(text) {
+    text = text.toLowerCase();
+    for (i = 0; i < text.length; i++) {
+        if (letters.indexOf(text.charAt(i), 0) != -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasNumbers(text) {
+    for(i = 0; i < text.length; i++) {
+        if (numbers.indexOf(text.charAt(i),0) != -1) {
+            return true;
+        }
     }
     return false;
 }
@@ -74,28 +74,28 @@ function hasNumbers(text){
 function validateInputName() {
     const userNameValue = userName.value.trim();
     if (userNameValue === '') {
-        const textError = "Name is required."
+        const textError = "Name is required.";
         setError(userName, textError);
         return alertText = textError;
     } else if (userNameValue.length < 3 ) {
-        const textError = "Name must be at least 3 character."
-        setError(userName, textError)
+        const textError = "Name must be at least 3 character.";
+        setError(userName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(userNameValue, false)) {
-        const textError = "Name cant contain special characters."
-        setError(userName, textError)
+        const textError = "Name cant contain special characters.";
+        setError(userName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasNumbers(userNameValue) === true) {
-        const textError = "Name cant contain numbers."
-        setError(userName, textError)
+        const textError = "Name cant contain numbers.";
+        setError(userName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(userName);
         alertText = userNameValue;
-        return alertText
+        return alertText;
     }
 }
 
@@ -107,205 +107,205 @@ function validateInputLastname() {
         return alertText = textError;
     } else if (lastNameValue.length < 3 ) {
         const textError = "Lastname must be at least 3 character."
-        setError(lastName, textError)
+        setError(lastName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(lastNameValue, false)) {
-        const textError = "Lastname cant contain special characters."
-        setError(lastName, textError)
+        const textError = "Lastname cant contain special characters.";
+        setError(lastName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasNumbers(lastNameValue) === true) {
-        const textError = "Lastname cant contain numbers."
-        setError(lastName, textError)
+        const textError = "Lastname cant contain numbers.";
+        setError(lastName, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(lastName);
         alertText = lastNameValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputDni() {
     const dniValue = dni.value.trim();
     if (dniValue === '') {
-        const textError = "Dni is required."
+        const textError = "Dni is required.";
         setError(dni, textError);
         return alertText = textError;
     } else if (dniValue.length < 8 ) {
-        const textError = "Dni must be at least 8 characters long."
+        const textError = "Dni must be at least 8 characters long.";
         setError(dni, textError)
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(dniValue, true)) {
-        const textError = "Dni cant contain special characters."
-        setError(dni, textError)
+        const textError = "Dni cant contain special characters.";
+        setError(dni, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasLetters(dniValue) === true) {
-        const textError = "Dni cant contain letters."
-        setError(dni, textError)
+        const textError = "Dni cant contain letters.";
+        setError(dni, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(dni);
         alertText = dniValue;
-        return alertTextDniSuccess
+        return alertTextDniSuccess;
     }
 }
 
 function validateInputTelephone() {
     const telephoneValue = telephone.value.trim();
     if (telephoneValue === '') {
-        const textError = "Telephone is required."
+        const textError = "Telephone is required.";
         setError(telephone, textError);
         return alertText = textError;
     } else if (telephoneValue.length != 10 ) {
         const textError = "Telephone must be at least 10 characters long."
-        setError(telephone, textError)
+        setError(telephone, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(telephoneValue, true)) {
         const textError = "Telephone cant contain special characters."
-        setError(telephone, textError)
+        setError(telephone, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasLetters(telephoneValue) === true) {
-        const textError = "Telephone cant contain letters."
-        setError(telephone, textError)
+        const textError = "Telephone cant contain letters.";
+        setError(telephone, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(telephone);
         alertText = telephoneValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputAddress() {
     const addressValue = address.value.trim();
     if (addressValue === '') {
-        const textError = "Address is required."
+        const textError = "Address is required.";
         setError(address, textError);
         return alertText = textError;
     } else if (addressValue.length < 5 ) {
-        const textError = "Address must be at least 5 character."
-        setError(address, textError)
+        const textError = "Address must be at least 5 character.";
+        setError(address, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(addressValue, false)) {
-        const textError = "Address cant contain special characters."
-        setError(address, textError)
+        const textError = "Address cant contain special characters.";
+        setError(address, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (!hasLetters(addressValue) === true || !hasNumbers(addressValue) === true) {
-        const textError = "Address must contain numbers and letters."
-        setError(address, textError)
+        const textError = "Address must contain numbers and letters.";
+        setError(address, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(address);
         alertText = addressValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputLocation() {
     const locValue = loc.value.trim();
     if (locValue === '') {
-        const textError = "Location is required."
+        const textError = "Location is required.";
         setError(loc, textError);
         return alertText = textError;
     } else if (locValue.length < 3 ) {
-        const textError = "Location must be at least 3 character."
-        setError(loc, textError)
+        const textError = "Location must be at least 3 character.";
+        setError(loc, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(locValue, false)) {
-        const textError = "Location cant contain special characters."
-        setError(loc, textError)
+        const textError = "Location cant contain special characters.";
+        setError(loc, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(loc);
         alertText = locValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputAreaCode() {
     const areaCodeValue = areaCode.value.trim();
     if (areaCodeValue === '') {
-        const textError = "Area code is required."
+        const textError = "Area code is required.";
         setError(areaCode, textError);
         return alertText = textError;
     } else if (areaCodeValue.length < 4 || areaCodeValue.length > 5 ) {
-        const textError = "Area code must be between 4 and 5."
-        setError(areaCode, textError)
+        const textError = "Area code must be between 4 and 5.";
+        setError(areaCode, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(areaCodeValue, true)) {
-        const textError = "Area code cant contain special characters."
-        setError(areaCode, textError)
+        const textError = "Area code cant contain special characters.";
+        setError(areaCode, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasLetters(areaCodeValue) === true) {
-        const textError = "Area code cant contain letters."
-        setError(areaCode, textError)
+        const textError = "Area code cant contain letters.";
+        setError(areaCode, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(areaCode);
         alertText = areaCodeValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputEmail() {
     const emailValue = email.value.trim();
     if (emailValue === '') {
-        const textError = "Email is required."
+        const textError = "Email is required.";
         setError(email, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (!isValidEmail(emailValue)) {
-        const textError = "Provide a valid email address."
+        const textError = "Provide a valid email address.";
         setError(email, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(email);
         alertText = emailValue;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputPassword() {
     const passwordValue = password.value.trim();
     if (passwordValue === '') {
-        const textError = "Password is required."
+        const textError = "Password is required.";
         setError(password, textError);
         return alertText = textError;
     } else if (passwordValue.length < 8 ) {
-        const textError = "Password must be at least 8 characters."
-        setError(password, textError)
+        const textError = "Password must be at least 8 characters.";
+        setError(password, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (hasSpecialCharacters(passwordValue) || passwordValue.indexOf(' ') > 0) {
-        const textError = "Password cant contain special characters."
-        setError(password, textError)
+        const textError = "Password cant contain special characters.";
+        setError(password, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else if (!hasLetters(passwordValue) === true || !hasNumbers(passwordValue) === true) {
-        const textError = "Password must contain numbers and letters."
-        setError(password, textError)
+        const textError = "Password must contain numbers and letters.";
+        setError(password, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     } else {
         setSuccess(password);
         alertText = passwordValue;
-        return alertText
+        return alertText;
     }
 }
 
@@ -313,39 +313,39 @@ function validateInputPasswordRepeat() {
     const passwordRepeatValue = passwordRepeat.value.trim();
     const passwordValue = password.value.trim();
     if (passwordRepeatValue === '') {
-        const textError = "Password repetition is required."
+        const textError = "Password repetition is required.";
         setError(passwordRepeat, textError);
         return alertText = textError;
     } else if (passwordRepeatValue === passwordValue) {
         setSuccess(passwordRepeat);
         alertText = passwordRepeatValue;
-        return alertText
+        return alertText;
     } else {
-        const textError = "Password do not match."
-        setError(passwordRepeat, textError)
+        const textError = "Password do not match.";
+        setError(passwordRepeat, textError);
         alertText = textError;
-        return alertText
+        return alertText;
     }
 }
 
 function validateInputBirthday() {
     const birthdayValue = birthday.value.trim();
-    var dateEl = birthdayValue.split('-')
-    var formattedDate = dateEl[2] + '/' + dateEl[1] + '/' + dateEl[0]
+    var dateEl = birthdayValue.split('-');
+    var formattedDate = dateEl[2] + '/' + dateEl[1] + '/' + dateEl[0];
     if (birthdayValue === '') {
-        const textError = "Birthday is required."
+        const textError = "Birthday is required.";
         setError(birthday, textError);
         return alertText = textError;
     } else {
         setSuccess(birthday);
         alertText = formattedDate;
-        return alertText
+        return alertText;
     }
 }
 
 function submitForm() {
-    var innerAlert = ""
-    validateInputName()
+    var innerAlert = "";
+    validateInputName();
     innerAlert += alertText + "\n";
     validateInputLastname();
     innerAlert += alertText + "\n";
